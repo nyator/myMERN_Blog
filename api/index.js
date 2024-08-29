e955c32898e2feb2a9e56e41ed456b307aa505ee
Henry <hhen5562@gmail.com>
2024-08-29 03:00:04 +0000

    mongoDB and Mongoose >>> SETUP

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const cors = require('cors')
+const mongoose  = require('mongoose')
 const app = express()
 const port = 3000
 
+app.use(cors())
+app.use(express.json())
+
+await mongoose.connect('mongodb+srv://blog:<4Mdix4lTqfKw92FT>@cluster0.6go5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
 app.post('/register', (req, res) => {
-  res.json('test ok!')
+    const { username, password } = req.body
+  res.json({requestData:{username,password}})
 })
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
+
