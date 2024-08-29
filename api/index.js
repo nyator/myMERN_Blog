c991d4f0a90e6bb8079d4210e9ae7f8128c1e9af
Henry <hhen5562@gmail.com>
2024-08-29 01:46:39 +0000

    api

--- api/index.js
+++ api/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/register', (req, res) => {
+  res.json('test ok!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
