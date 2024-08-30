0527f8d21c9882d81f88be24daa55e3e4c54d22d
Henry <hhen5562@gmail.com>
2024-08-30 01:51:47 +0000

    add

--- api/index.js
+++ api/index.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose  = require('mongoose')
+const {MongoClient} = require("mongodb");
 const app = express()
 const port = 3000
+
+const User = require('./models/User')
 
 app.use(cors())
 app.use(express.json())
 
-await mongoose.connect('mongodb+srv://blog:<4Mdix4lTqfKw92FT>@cluster0.6go5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+MongoClient.connect('mongodb+srv://blog:4Mdix4lTqfKw92FT@cluster0.6go5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
     const { username, password } = req.body
-  res.json({requestData:{username,password}})
+    const userDoc = await User.create({ username, password})
+  res.json(userDoc)
 })
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
 
+
+// mongodb+srv://blog:<4Mdix4lTqfKw92FT>@cluster0.6go5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
