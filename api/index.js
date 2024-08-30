b38d7291c4cf61626364375109e1bad75ccfdb63
Henry <hhen5562@gmail.com>
2024-08-30 10:33:13 +0800

    coonected sucessfully

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const {MongoClient} = require("mongodb");
+const mongoose = require("mongoose");
 const app = express()
 const port = 3000
 
@@ -9,7 +10,7 @@ const User = require('./models/User')
 app.use(cors())
 app.use(express.json())
 
-MongoClient.connect('mongodb+srv://blog:4Mdix4lTqfKw92FT@cluster0.6go5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://blog:4Mdix4lTqfKw92FT@cluster0.6go5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body
